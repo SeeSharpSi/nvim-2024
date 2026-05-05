269550b9c06ec80ace1b11057a0a123e5dbb680d
Silas Tompkins <silastompkins@outlook.com>
2026-05-05 00:16:18 -0400

    removed cmp preselect

--- lua/silas/plugins/cmp.lua
+++ lua/silas/plugins/cmp.lua
@@ -17,6 +17,7 @@ return {
     local cmp = require('cmp')
 
     cmp.setup({
+      preselect = cmp.PreselectMode.None,
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
@@ -32,7 +33,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
